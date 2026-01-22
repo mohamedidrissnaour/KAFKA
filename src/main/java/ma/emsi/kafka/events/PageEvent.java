ac3014622bdea9c1abc3ef55d932024ab1149c1f
Mohamed-Idriss Naour <104274703+simonaour@users.noreply.github.com>
2026-01-22 23:40:26 +0100

    first

---
+++ src/main/java/ma/emsi/kafka/events/PageEvent.java
@@ -0,0 +1,9 @@
+package ma.emsi.kafka.events;
+
+import java.util.Date;
+
+public record PageEvent(String name,
+                        String user,
+                        Date date,
+                        long duration
+                        ){}
